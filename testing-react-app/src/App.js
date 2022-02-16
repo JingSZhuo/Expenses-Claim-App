666350a7a83d29b12fa1bcef9b33da3e0d36a844
GROUP-26-ECS506U-SE-Project <98566140+GROUP-26-ECS506U-SE-Project@users.noreply.github.com>
2022-02-16 21:57:35 +0000

    Update App.js

--- testing-react-app/src/App.js
+++ testing-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to save lol 11.
+          Edit <code>src/App.js</code> and save to save lol 6969.
         </p>
         <a
           className="App-link"
