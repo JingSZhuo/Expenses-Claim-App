d2d18e0004b082a1a73b62b0051b68ee96245149
jing2 <jing2001@live.co.uk>
2022-02-17 00:23:59 +0000

    testing

--- testing-react-app/src/App.js
+++ testing-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to save lol 6969.
+          Edit <code>src/App.js</code> and save to save XDDDDDDDDDDDDDD.
         </p>
         <a
           className="App-link"
