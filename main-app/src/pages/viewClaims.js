3cc5e93f0beebdca644cc9eb8476b134c826a1c2
jing2 <jing2001@live.co.uk>
2022-03-11 13:07:24 +0000

    .

--- main-app/src/pages/viewClaims.js
+++ main-app/src/pages/viewClaims.js
@@ -1,23 +1,59 @@
-import { useState } from "react";
-//import { Link } from 'react-router-dom';
+import { useEffect, useState, } from "react";
+import db from "../firebase";
 import { onAuthStateChanged, getAuth} from "firebase/auth";
+import { collection, getDoc ,getDocs, onSnapshot, doc } from "firebase/firestore";
 
 function StatusIn(){
-    return(<h2>Logged in!</h2>)
+
+
+    //Single Document
+    const docRef = doc(db, "Employee", "User2")
+
+    onSnapshot(docRef, (doc) => {
+      console.log(doc.data(), doc.id)   //shove into useState?? or var then use map?
+    })
+
+
+    const [data, getData] = useState([])
+    const usersCollectionRef = collection(db, "Employee")
+
+    useEffect(() => {
+      const getData1 = async () => {
+        const data_1 = await getDocs(usersCollectionRef);
+        getData(data_1.docs.map((doc1) => ({...doc1.data(), id: doc1.id })))
+      }
+      getData1()
+    }, [])
+    
+    return(
+    <>
+        <h2>Logged in!</h2>
+        <div>
+          {data.map((testing) => {
+            return (
+              <div>
+                <a>Data: {testing.Claim1}</a>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    )
 }
+
 function StatusOut() {
-    return(<h2>ERROR!!!</h2>)
+    return(<h2>Not Logged In!!!</h2>)
 }
  
 function Status() {
-    const  [loginStatus, setLoginStatus] = useState("false")
+    const  [loginStatus, setLoginStatus] = useState(false)
   
     const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, (user) => {          //Check if user is logged in
       if (user) {
-        setLoginStatus("true"); console.log("!!!")
+        setLoginStatus(true); console.log("!!!")
       } else {
-        setLoginStatus("false"); console.log("???")
+        setLoginStatus(false); console.log("???")
       }
     })
     
@@ -26,12 +62,10 @@ function Status() {
 
 const viewClaim = () => {
 
-      //<h2>{ Status.loginStatus === "true" ? console.log("Logged in") : console.log("Logged out")}</h2>
-
     return (  
         <body>
-            <h1>claims</h1>
-                { Status() === "true" ?  <StatusIn/> : <StatusOut/>}
+            <h1>Claims page</h1>
+                { Status() === true ?  <StatusIn/> : <StatusOut/>}
         </body>
 
     );
