8f02bec4c66a2a1b24785f9467a7b8268ef51961
sesame <navidsatar18@gmail.com>
2022-04-04 00:09:05 +0100

    login changes

--- main-app/src/pages/LoginSignUp.js
+++ main-app/src/pages/LoginSignUp.js
@@ -12,7 +12,6 @@ import { faCaretDown} from '@fortawesome/free-solid-svg-icons';
 
 
 
-
 function Login_Signup() {
 
     //Hook states
@@ -80,7 +79,7 @@ function Login_Signup() {
 
      return (  
         <>
-
+       
         <nav className="navbar">
             <Link className='navbuttons' to="/" >Home</Link>
             <Link className='navbuttons' to="/about" >About</Link>
@@ -95,37 +94,66 @@ function Login_Signup() {
             </div> 
             <Link className='loginsignupbutton' to="/LoginSignup">Login and Sign-Up</Link>
           </nav>
-            <h1>Login | Sign Up </h1>
-            <br></br>
+
+          
+          <link rel="stylesheet" type="text/css" href="src/bootstrap.min.css"></link>
+    <meta charset="UTF-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <title>ClaimIT</title>
+   
+    <body>
+        <div className ="form-body">
+            <div className="form-holder"> 
+                <div className="form-content">
+                    <div className="form-items">
+                        <h3>Reinvent your expense reporting.</h3> 
+                        <p>Never leave your expense reports to your desk.</p>
+                        
+                        <div class="page-links">
+                                        <a href="" class="active">Login</a>
+                        </div>
+
+                       
 
 
-            <form className='LS'>
-                <h3>Signup</h3>
-                <input type="email" placeholder='Email...' onChange={(event) => {setRegisterEmail(event.target.value)}} required/>
-                <input placeholder='Password...' onChange={(event) => {setRegisterPassword(event.target.value)}} required/>
-                <input placeholder='Confirm password...' required/>
+                        <form>
+                            <h3>Employee/Staff Login</h3>
+                            <input placeholder='Email...' onChange={(event) => {setLoginEmail(event.target.value)}}/>
+                            <input placeholder='Password...' onChange={(event) => {setLoginrPassword(event.target.value)}}/>
+                            <div className="form-button">
+                            
+                            <button type ="button" id="submit" onClick={login} className="ibtn" value={"Login"}>Login</button>
+                            </div>
+                        </form>
 
-                <input type="button" onClick={register} value={"Signup"} ></input>
-            </form>
+                        <div class="page-links">
+                                        <a href="" class="active">Register</a>
+                        </div>
+
+                        <form>
+                            <h3>Signup</h3>
+                            <input className ="form-control" type="email" placeholder='Email...' onChange={(event) => {setRegisterEmail(event.target.value)}} required/>
+                            <input className ="form-control" placeholder='Password...' onChange={(event) => {setRegisterPassword(event.target.value)}} required/>
+                            <input className ="form-control" placeholder='Confirm password...' required/>
+                            <div className="form-button">
+                            <button id="submit" className="ibtn" type="button" onClick={register} value={"Signup"} >Sign Up</button>
+                            </div>
+                        </form>
+        
+                        <div className='LS'>
+                            <h3>User - logged in</h3>
 
 
-            <form className='LS'>
-                <h3>Employee/Staff Login</h3>
-                <input placeholder='Email...' onChange={(event) => {setLoginEmail(event.target.value)}}/>
-                <input placeholder='Password...' onChange={(event) => {setLoginrPassword(event.target.value)}}/>
-
-                <input type="button" onClick={login} value={"Login"}></input>
-            </form>
-
-            <div className='LS'>
-                <h3>User - logged in</h3>
-
-
-                {/* <button onClick={logout}>Logout</button> */}
+                            {/* <button onClick={logout}>Logout</button> */}
+                        </div>
+                        <br/>
+                            Logged in as:  {user?.email}
+                        <br/>
+                    </div>
+                </div>
             </div>
-            <br/>
-                Logged in as:  {user?.email}
-            <br/>
+        </div>
+    </body>
         </>
         );
     }
