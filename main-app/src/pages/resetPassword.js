9e48deea333938db2b027123db8c1d7f5f0d57ea
JingSZhuo <jing2001@live.co.uk>
2022-04-10 01:32:59 +0100

    fixed positioning in login page

--- main-app/src/pages/resetPassword.js
+++ main-app/src/pages/resetPassword.js
@@ -1,5 +1,6 @@
 import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import BackgroundParticles from './BackgroundParticles';
+import {Link} from "react-router-dom";
 
 const PasswordResetPage = () => {
 
@@ -18,16 +19,23 @@ const PasswordResetPage = () => {
     } 
 
     return ( 
-        <>
+
 
         <body>
+
+            <nav className="navbar">
+                <Link className='navbuttons' to="/" >Home</Link>
+                <Link className='navbuttons' to="/about" >About</Link>
+                <Link className='loginsignupbutton' to="/LoginSignup">Login and Sign-Up</Link>
+            </nav>
+
             <BackgroundParticles />
 
             <div class="form-container">
                 <form class="form-wrap">
                     <h2>Forgot Password</h2>
                         <div class="form-box">
-                            <input id='emailpasswordreset' type="text" placeholder="Enter Email" />
+                            <input id='emailpasswordreset' className='input' type="text" placeholder="Enter Email" />
                         </div>
                     <div class="form-submit">
                         <button className="" type="button" value="Send" onClick={resetPassword}> Send </button> 
@@ -35,8 +43,6 @@ const PasswordResetPage = () => {
                 </form>
             </div>
         </body>
-        
-        </>
      );
 }
  
